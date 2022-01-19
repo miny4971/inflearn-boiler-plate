41c46ddfb588755fd6f15078690fb08c57518790
miny4971 <miny4971@gmail.com>
2022-01-19 21:59:04 +0900

    비밀 정보 보호(mongoURI)

---
+++ config/prod.js
@@ -0,0 +1,4 @@
+//heroku
+module.exports = {
+    mongoURI: process.env.MONGO_URI
+}
--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 const express = require('express')
 const app = express()
 const port = 5000;
-
+const bodyParser = require('body-parser')
+const {User} = require("./models/User")
+const config = require('./config/key')
+//urlencoded 와 json타입으로 온 문장을 분석할 수 있게 해줌
+//이로써 req.body사용가능
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://miny4971:somin1121!@cluster0.vf9dr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+mongoose.connect(config.mongoURI,
 { useNewUrlParser: true, useUnifiedTopology: true //useCreateIndex:true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
-app.get('/', (req,res) => res.send('Hello World!'))
+app.get('/', (req,res) => res.send('Hello '))
 
+
+app.post('/register', (req,res) => {
+
+    //회원 가입 시 필요한 정보들을 client에서 가져오면 
+    //그것들을 데이터베이스에 넣어준다.
+    const user = new User(req.body)
+
+    user.save((err,doc) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success:true
+        })
+    })
+})
 app.listen(port, ()=> console.log(`Example app listening on port ${port}!`))
